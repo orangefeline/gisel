6b59254d02826fec13b4aac7114312cced029843
Roy:b <rodrigo.anothermen@gmail.com>
2024-01-05 18:33:18 -0600

    cambios

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-window.onload = function(alert) {
-    alert('¡Bienvenido a nuestra página!');
-};
+document.getElementById('whatsappButton').addEventListener('click', function() {
+    // Número de teléfono al que se enviará el mensaje (con código de país)
+    var phoneNumber = '+525572785113';
+  
+    // Mensaje que se enviará
+    var message = 'Hola mi nombre es ______________ confirmo mi asistencia al evento';
+  
+    // Construir la URL de WhatsApp
+    var whatsappURL = 'https://wa.me/' + 5572785113 + '?text=' + encodeURIComponent(message);
+  
+    // Abrir la ventana de WhatsApp
+    window.open(whatsappURL, '_blank');
+  });
+  
